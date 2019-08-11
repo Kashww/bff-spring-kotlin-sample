624fe0bf6e7da4e9e3ab8961c7ea977fd8aa619f
wataru-kashima <kashww.ks3@gmail.com>
2019-08-11 12:49:16 +0900

    first commit

---
+++ src/main/kotlin/com/kashww/bffspringkotlinsample/BffSpringKotlinSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.kashww.bffspringkotlinsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BffSpringKotlinSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<BffSpringKotlinSampleApplication>(*args)
+}
